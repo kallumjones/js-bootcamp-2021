19a10349c332c4f3335b7814176fc3201f614d85
Kallum Jones <kallum.jones@autotrader.co.uk>
2021-09-29 10:46:54 +0100

    Add HTML elements to RPS game

--- session-4/challenges/rockPaperScissors.js
+++ session-4/challenges/rockPaperScissors.js
@@ -10,8 +10,87 @@ You need to accept the user's choice (which for now can be stored in a variable)
 The computer choice can be derived frim the 'Math.random()' which returns a number between 0 and 1
 
 */
+//clear text box
+const clearTextBox = () => document.getElementById('userChoice').value = '';
+
+let rpsChoices = ['rock', 'paper', 'scissors'];
+
+const onClick = () => {
+
+    let compRand = Math.floor(Math.random() * 3);
+    let compChoice = rpsChoices[compRand];
+    let userChoice = document.getElementById('userChoice').value;
+    let userChoiceLower = userChoice.toLowerCase();
+    let resultsDiv = document.getElementById('results');
+
+    let resultsHTML;
+
+    if(rpsChoices.includes(userChoiceLower) === false) {
+        resultsHTML = 'Please Enter Valid Option (Rock, Paper, Scissors)'
+    } else {
+        resultsHTML = `Player Choice: ${userChoiceLower} <br>
+        Computer Choice: ${compChoice} <br>`;
+    }
+
+    if (userChoiceLower === 'rock') {
+        switch (compChoice) {
+          case 'rock':
+            resultsHTML = resultsHTML + 'Draw! <br> Play again!';
+            break;
+          case 'paper':
+            resultsHTML = resultsHTML + 'Computer has won! <br> Play again!';
+            break;
+          case 'scissors':
+            resultsHTML = resultsHTML + 'User has won! <br> Play again!';
+        }
+      } else if (userChoiceLower === 'paper') {
+        switch (compChoice) {
+          case 'rock':
+            resultsHTML = resultsHTML + 'User has won! <br> Play again!';
+            break;
+          case 'paper':
+            resultsHTML = resultsHTML + 'Draw! <br> Play again!';
+            break;
+          case 'scissors':
+            resultsHTML = resultsHTML + 'Computer has won! <br> Play again!';
+            break;
+        }
+      } else if (userChoiceLower === 'scissors') {
+        switch (compChoice) {
+          case 'rock':
+            resultsHTML = resultsHTML + 'Computer has won! <br> Play again!';
+            break;
+          case 'paper':
+            resultsHTML = resultsHTML + 'User has won! <br> Play again!';
+            break;
+          case 'scissors':
+            resultsHTML = resultsHTML + 'Draw! <br> Play again!';
+        }
+      }
+
+      clearTextBox();
+
+      resultsDiv.innerHTML = resultsHTML;
+
+}
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* ==========Basic, through console========
 const choices = ["Rock", "Paper", "Scissor"];
 
 let userChoice = choices[2]; // change number for user input
@@ -56,3 +135,4 @@ if (userChoice === "Rock") {
       console.log("Draw!");
   }
 }
+*/
